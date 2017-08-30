a24dd57ed118b921c6bca6d7bff9f3b9189a14b9
Andrew Glines <aglines-epicodus@gmail.com>
2017-08-29 17:50:30 -0700

    Deserialize an array of tweets into a List of Tweet objects

--- TrumpTweets/src/TrumpTweets/Controllers/HomeController.cs
+++ TrumpTweets/src/TrumpTweets/Controllers/HomeController.cs
@@ -25,14 +25,19 @@ namespace TrumpTweets.Controllers
 
         public void ConvertTweets()
         {
-            string firstTweet = @"{ 'source': 'Twitter for iPhone', 'id_str': '815271067749060609', 'text': 'RT @realDonaldTrump: Happy Birthday @DonaldJTrumpJr!\nhttps://t.co/uRxyCD3hBz', 'created_at': 'Sat Dec 31 18:59:04 +0000 2016', 'retweet_count': 9529, 'in_reply_to_user_id_str': null, 'favorite_count': 0, 'is_retweet': true}";
-            Tweet deserializedTweet = JsonConvert.DeserializeObject<Tweet>(firstTweet);
+            //this is test data, will need to use actual file
+            string tweets = @"[{'source': 'Twitter for iPhone', 'id_str': '815271067749060609', 'text': 'RT @realDonaldTrump: Happy Birthday @DonaldJTrumpJr!\nhttps://t.co/uRxyCD3hBz', 'created_at': 'Sat Dec 31 18:59:04 +0000 2016', 'retweet_count': 9529, 'in_reply_to_user_id_str': null, 'favorite_count': 0, 'is_retweet': true},{'source': 'Twitter for iPhone', 'id_str': '814920722208296960', 'text': 'Join @AmerIcan32, founded by Hall of Fame legend @JimBrownNFL32 on 1/19/2017 in Washington, D.C.\u2026 https://t.co/9WJZ8iTCQV', 'created_at': 'Fri Dec 30 19:46:55 +0000 2016', 'retweet_count': 7366, 'in_reply_to_user_id_str': null, 'favorite_count': 25336, 'is_retweet': false}]";
 
-            System.Diagnostics.Debug.WriteLine("**************\n**************\n**************\n");
+            List<Tweet> list = JsonConvert.DeserializeObject<List<Tweet>>(tweets);
+
+            //process data
+            foreach (Tweet item in list)
+            {
+                System.Diagnostics.Debug.WriteLine("**************\n**************\n**************  " + item.created_at);
+            }
+
+            System.Diagnostics.Debug.WriteLine(list.Count);
             
-            System.Diagnostics.Debug.WriteLine(deserializedTweet.source.ToString());
-
-
         }
     }
 
--- TrumpTweets/src/TrumpTweets/Models/Tweet.cs
+++ TrumpTweets/src/TrumpTweets/Models/Tweet.cs
@@ -12,11 +12,11 @@ namespace TrumpTweets.Models
         public string source { get; set; }
         public string id_str { get; set; }
         public string text { get; set; }
-        public string createdAt { get; set; }
-        public string retweetCount { get; set; }
-        public string inReplyToUser { get; set; }
-        public string favoriteCount { get; set; }
-        public Boolean isRetweet { get; set; }
+        public string created_at { get; set; }
+        public string retweet_count { get; set; }
+        public string in_reply_to_user_id_str { get; set; }
+        public string favorite_count { get; set; }
+        public Boolean is_retweet { get; set; }
     }
     
 }

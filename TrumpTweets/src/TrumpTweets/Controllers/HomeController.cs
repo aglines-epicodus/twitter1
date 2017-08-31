6e059e3c84a45bbb625e0f30da5e856628f08e93
Andrew Glines <aglines-epicodus@gmail.com>
2017-08-31 11:52:03 -0700

    Refine color gradients, adjust stopword list

--- TrumpTweets/src/TrumpTweets/Controllers/HomeController.cs
+++ TrumpTweets/src/TrumpTweets/Controllers/HomeController.cs
@@ -15,12 +15,11 @@ namespace TrumpTweets.Controllers
 {
     public class HomeController : Controller
     {
-
         //Actual file data probably needs a relative path, won't work on other computers?
         static string file = @"C:\Users\Andrew Glines\Source\Repos\twitter1\TrumpTweets\src\TrumpTweets\realDonaldTrump2016tweets.json";
         static string tweets = System.IO.File.ReadAllText(file);
 
-        // Test data - 
+        //Test data - 
         //static string tweets = @"[{'source': 'Twitter for iPhone', 'id_str': '815271067749060609', 'text': 'RT RT @realDonaldTrump: Happy Happy Birthday @DonaldJTrumpJr!\nhttps://t.co/uRxyCD3hBz', 'created_at': 'Sat Dec 31 18:59:04 +0000 2016', 'retweet_count': 9529, 'in_reply_to_user_id_str': null, 'favorite_count': 0, 'is_retweet': true},{'source': 'Twitter for Android', 'id_str': '814920722208296960', 'text': 'Happy Join @AmerIcan32, founded by Hall of Fame legend legend @JimBrownNFL32 onsin Washington, D.C.\u2026 https://t.co/9WJZ8iTCQV', 'created_at': 'Fri Dec 30 19:46:55 +0000 2016', 'retweet_count': 7366, 'in_reply_to_user_id_str': null, 'favorite_count': 25336, 'is_retweet': false}]";
         
         // Deserialize list of tweet objects
@@ -50,15 +49,15 @@ namespace TrumpTweets.Controllers
 
             // we need a stop list of words to not count
             // first list will not include pronouns
-            //List<string> StopList = new List<string> { "rt", "http", "https", "https:", "co", "com", "in", "at", "will", "to", "the", "a", "an", "and", "of", "for", "on", "it", "was", "is", "be", "have", "are", "that", "as", "was", "were", "by", "out", "if"};
+            //List<string> StopList = new List<string> { "rt", "http", "https", "https:", "co", "com", "in", "at", "will", "to", "the", "a", "an", "and", "of", "for", "on", "it", "was", "is", "be", "have", "are", "that", "as", "was", "were", "by", "out", "if", "but", "or", "with"};
             // another list includes pronouns
-            List<string> StopList = new List<string> { "me", "i", "my", "he", "him", "his", "she", "her", "hers", "you", "u", "your", "yours", "we", "our", "ours", "they", "their", "theirs", "rt", "http", "https", "https:", "co", "com", "in", "at", "will", "to", "the", "a", "an", "and", "of", "for", "on", "it", "was", "is", "be", "have", "are", "that", "as", "was", "were", "by", "out", "if" };
+            List<string> StopList = new List<string> { "me", "i", "my", "he", "him", "his", "she", "her", "hers", "you", "u", "your", "yours", "we", "our", "ours", "they", "their", "theirs", "rt", "http", "https", "https:", "co", "com", "in", "at", "will", "to", "the", "a", "an", "and", "of", "for", "on", "it", "was", "is", "be", "have", "are", "that", "as", "was", "were", "by", "out", "if", "but", "or", "with" };
 
             foreach (Tweet item in list)
             {
                 string textToProcess = item.text;
                 string textLowered = textToProcess.ToLower();
-                string[] source = textLowered.Split(new string[] {" ", ".", ",", "!", "?", "#", "/", "-", "&", "(", ")" }, StringSplitOptions.RemoveEmptyEntries);
+                string[] source = textLowered.Split(new string[] {" ", ".", ",", "!", "?", "#", "/", "-", "&", "(", ")", "\""    }, StringSplitOptions.RemoveEmptyEntries);
 
                 foreach (string word in source)
                     if (!StopList.Contains(word)) {
@@ -66,12 +65,10 @@ namespace TrumpTweets.Controllers
                             if (!wordCounts.ContainsKey(word))
                             {
                                 wordCounts.Add(word, 1);
-                                //System.Diagnostics.Debug.WriteLine("11111111111 new word " + word);
                             }
                             else
                             {
                                 wordCounts[word] = wordCounts[word] + 1;
-                                //System.Diagnostics.Debug.WriteLine("2222222222 repeated word " + word + " count is now " + wordCounts[word]);
                             }
                         }
                 }
@@ -86,6 +83,8 @@ namespace TrumpTweets.Controllers
             var wordsForCloud = new List<CloudWord>();
             foreach (var thing in wordCounts)
             {
+                // create a dict of items and their counts for wordcloud
+                // use a multiplier to ensure all counts show up
                 wordsForCloud.Add(new CloudWord() { text = thing.Key, size = thing.Value*5 });
             }
 
@@ -95,7 +94,6 @@ namespace TrumpTweets.Controllers
 
         }
         //System.Diagnostics.Debug.WriteLine(list.Count);
-
 
         public string AvgLengthOfTweetBySource()
             {
@@ -122,9 +120,6 @@ namespace TrumpTweets.Controllers
             return logAvgBySource;
 
         }
-
-
-
     }
 }
 

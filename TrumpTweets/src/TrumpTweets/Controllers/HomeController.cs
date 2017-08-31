2124fc3cf46d25a71e847ea68ba2e22bdad54c50
Andrew Glines <aglines-epicodus@gmail.com>
2017-08-30 22:14:37 -0700

    Calculates average length of tweet for both sources (iphone and android)

--- TrumpTweets/src/TrumpTweets/Controllers/HomeController.cs
+++ TrumpTweets/src/TrumpTweets/Controllers/HomeController.cs
@@ -22,15 +22,15 @@ namespace TrumpTweets.Controllers
         //static string tweets = System.IO.File.ReadAllText(file);
 
         // use test data while coding isntead
-        static string tweets = @"[{'source': 'Twitter for iPhone', 'id_str': '815271067749060609', 'text': 'RT RT @realDonaldTrump: Happy Happy Birthday @DonaldJTrumpJr!\nhttps://t.co/uRxyCD3hBz', 'created_at': 'Sat Dec 31 18:59:04 +0000 2016', 'retweet_count': 9529, 'in_reply_to_user_id_str': null, 'favorite_count': 0, 'is_retweet': true},{'source': 'Twitter for iPhone', 'id_str': '814920722208296960', 'text': 'Happy Join @AmerIcan32, founded by Hall of Fame legend legend @JimBrownNFL32 onsin Washington, D.C.\u2026 https://t.co/9WJZ8iTCQV', 'created_at': 'Fri Dec 30 19:46:55 +0000 2016', 'retweet_count': 7366, 'in_reply_to_user_id_str': null, 'favorite_count': 25336, 'is_retweet': false}]";
+        static string tweets = @"[{'source': 'Twitter for iPhone', 'id_str': '815271067749060609', 'text': 'RT RT @realDonaldTrump: Happy Happy Birthday @DonaldJTrumpJr!\nhttps://t.co/uRxyCD3hBz', 'created_at': 'Sat Dec 31 18:59:04 +0000 2016', 'retweet_count': 9529, 'in_reply_to_user_id_str': null, 'favorite_count': 0, 'is_retweet': true},{'source': 'Twitter for Android', 'id_str': '814920722208296960', 'text': 'Happy Join @AmerIcan32, founded by Hall of Fame legend legend @JimBrownNFL32 onsin Washington, D.C.\u2026 https://t.co/9WJZ8iTCQV', 'created_at': 'Fri Dec 30 19:46:55 +0000 2016', 'retweet_count': 7366, 'in_reply_to_user_id_str': null, 'favorite_count': 25336, 'is_retweet': false}]";
         static List<Tweet> list = JsonConvert.DeserializeObject<List<Tweet>>(tweets);
 
         string log = "";
 
         public IActionResult Index()
         {
-            WordCounts();
-
+            //WordCounts();
+            AvgLengthOfTweetBySource();
             // method store text data from each tweet
             // render this data in View for a small number of those tweets
             ViewData["log"] = log;
@@ -78,9 +78,35 @@ namespace TrumpTweets.Controllers
 
             return log;
 
+        }
+        //System.Diagnostics.Debug.WriteLine(list.Count);
 
-            }
-            //System.Diagnostics.Debug.WriteLine(list.Count);
+
+        public string AvgLengthOfTweetBySource()
+            {
+                List<int> tweetLengthsIphone = new List<int>();
+                List<int> tweetLengthsAndroid = new List<int>();
+                foreach (Tweet item in list)
+                {
+                 if (item.source == "Twitter for iPhone")
+                    {
+                        tweetLengthsIphone.Add(item.text.Length);
+                    }
+                if (item.source == "Twitter for Android")
+                    {
+                        tweetLengthsAndroid.Add(item.text.Length);
+                    }
+                }
+
+            double avgLengthOfTweetIphone = tweetLengthsIphone.Average();
+            double avgLengthOfTweetAndroid = tweetLengthsAndroid.Average();
+
+            log += "iphone average is now  " + avgLengthOfTweetIphone + "\n";
+            log += "android average is now  " + avgLengthOfTweetAndroid + "\n";
+
+            return log;
+
         }
     }
+}
 
